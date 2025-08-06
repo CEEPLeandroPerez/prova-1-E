eacdb10e7c3ac8573fae9d304b4822cc9adf487c
CEEPLeandroPerez <leandro.perez@escola.pr.gov.br>
2025-08-06 08:50:47 -0300

    Add files via upload

--- jogo_advinhaçao1E.py
+++ jogo_advinhaçao1E.py
@@ -2,18 +2,19 @@ print("Leandro Hilebrand Perez")
 print("prova de introduçao a programaçao")
 
 import random
+import os
 
-import os 
+erros = 0
+sorteado = random.randrange(0, 100)
+jogador = int(input("Digite seu número!"))
 
-erros=0
-sorteado=random.randrange(0,100)
-jogador=int(input("Digite seu número!"))
-while(sorteado!=jogador):
-    os.system('cls')
-    if(sorteado>jogador):
+while sorteado != jogador:
+    os.system('cls')  # Limpa a tela
+    if sorteado > jogador:
         print("ERRO, o número é maior")
-    elif(sorteado<jogador):
+    elif sorteado < jogador:
         print("ERRO, o número é menor")
-        erros+=1
-        jogador=int(input("Digite seu número:"))
-        print("Número" + str(jogador) + "você acertou em :" + str(erros+1) + "tentativas")
+    erros += 1  # Conta a tentativa errada
+    jogador = int(input("Digite seu número:"))
+
+print(f"Você acertou o número {sorteado} em {erros + 1} tentativas.")
