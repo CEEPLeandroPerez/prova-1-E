5234eb023915478f982029eb2426a55d7c46a3ef
CEEPLeandroPerez <leandro.perez@escola.pr.gov.br>
2025-08-06 08:24:47 -0300

    Add files via upload

---
+++ calculadora maluca.py
@@ -0,0 +1,8 @@
+print ("Leandro Hillebrand Perez")
+print ("prova de introduçao a programaçao")
+
+print ("vamos multiplicar dois numeros!")
+num1 = float (input("digite o primeiro numero:"))
+num2 = float (input("digite o segundo numero:"))
+multiplicaçao = num1 * num2
+print (f"a multiplicaçao de {num1} e {num2} e {multiplicaçao}")
